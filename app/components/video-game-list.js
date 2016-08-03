2ac5b79059c72e149d1553e8466819b8dbf50e07
Josh Justice <jjustice@bignerdranch.com>
2016-08-03 09:39:53 -0400

    Add editing

--- app/components/video-game-list.js
+++ app/components/video-game-list.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    editGame(game) {
+      game.set('beingEdited', true);
+    },
+    saveGame(game) {
+      this.get('saveGame')(game, () => {
+        game.set('beingEdited', false);
+      });
+    },
+    cancelEditing(game) {
+      this.get('cancelEditing')(game, () => {
+        game.set('beingEdited', false);
+      });
+    },
     deleteGame(game) {
       if(confirm(`Are you sure you want to delete ${game.get('title')}?`)) {
         this.get('deleteGame')(game);
--- app/controllers/video-games.js
+++ app/controllers/video-games.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    saveGame(game, callback) {
+      game.save().then(() => callback())
+                 .catch(() => alert("Error saving game."));
+    },
+    cancelEditing(game, callback) {
+      game.rollbackAttributes();
+      callback();
+    },
     deleteGame(game) {
       game.destroyRecord().catch(() => alert("Error deleting game."));
     }
